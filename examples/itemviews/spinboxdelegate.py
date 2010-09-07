30e0bcdcbe114eeaf9b7fd9c0fcfab1c7ce606b5
Matti Airas <matti.p.airas@nokia.com>
2010-09-07 14:12:24 +0300

    merged spinboxdelegate.py from PyQt

--- examples/itemviews/spinboxdelegate.py
+++ examples/itemviews/spinboxdelegate.py
@@ -1,66 +1,59 @@
 #!/usr/bin/env python
 
-"""***************************************************************************
-**
-** Copyright (C) 2005-2005 Trolltech AS. All rights reserved.
-**
-** This file is part of the example classes of the Qt Toolkit.
-**
-** This file may be used under the terms of the GNU General Public
-** License version 2.0 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of
-** this file.  Please review the following information to ensure GNU
-** General Public Licensing requirements will be met:
-** http://www.trolltech.com/products/qt/opensource.html
-**
-** If you are unsure which license is appropriate for your use, please
-** review the following information:
-** http://www.trolltech.com/products/qt/licensing.html or contact the
-** sales department at sales@trolltech.com.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-**
-***************************************************************************"""
+############################################################################
+##
+## Copyright (C) 2005-2005 Trolltech AS. All rights reserved.
+##
+## This file is part of the example classes of the Qt Toolkit.
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following information to ensure GNU
+## General Public Licensing requirements will be met:
+## http://www.trolltech.com/products/qt/opensource.html
+##
+## If you are unsure which license is appropriate for your use, please
+## review the following information:
+## http://www.trolltech.com/products/qt/licensing.html or contact the
+## sales department at sales@trolltech.com.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+############################################################################
 
-"""
-    delegate.cpp
 
-    A delegate that allows the user to change integer values from the model
-    using a spin box widget.
-"""
-
-import sys
 from PySide import QtCore, QtGui
 
 
 class SpinBoxDelegate(QtGui.QItemDelegate):
-    def __init__(self, parent = None):
-        QtGui.QItemDelegate.__init__(self, parent)
-
     def createEditor(self, parent, option, index):
         editor = QtGui.QSpinBox(parent)
         editor.setMinimum(0)
         editor.setMaximum(100)
-        editor.installEventFilter(self)
 
         return editor
 
     def setEditorData(self, spinBox, index):
-        value = index.model().data(index, QtCore.Qt.DisplayRole)
+        value = index.model().data(index, QtCore.Qt.EditRole)
+
         spinBox.setValue(value)
 
     def setModelData(self, spinBox, model, index):
         spinBox.interpretText()
         value = spinBox.value()
 
-        model.setData(index, value)
+        model.setData(index, value, QtCore.Qt.EditRole)
 
     def updateEditorGeometry(self, editor, option, index):
         editor.setGeometry(option.rect)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
+    import sys
+
     app = QtGui.QApplication(sys.argv)
 
     model = QtGui.QStandardItemModel(4, 2)
@@ -73,7 +66,7 @@ if __name__ == "__main__":
     for row in range(4):
         for column in range(2):
             index = model.index(row, column, QtCore.QModelIndex())
-            model.setData(index, (row+1) * (column+1))
+            model.setData(index, (row + 1) * (column + 1))
 
     tableView.setWindowTitle("Spin Box Delegate")
     tableView.show()
