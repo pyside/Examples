777286a47cbee3439e439bb2cfa26bd5262c519c
Hugo Parente Lima <hugo.pl@gmail.com>
2011-01-19 14:22:34 -0200

    Fix mandelbrot example, so it doesn't exit without finishing the QThread.
    Reviewer: Lauro Moura <lauro.moura@openbossa.org>
              Renato Araújo <renato.filho@openbossa.org>

--- examples/threads/mandelbrot.py
+++ examples/threads/mandelbrot.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 
 ############################################################################
-# 
+#
 #  Copyright (C) 2004-2005 Trolltech AS. All rights reserved.
-# 
+#
 #  This file is part of the example classes of the Qt Toolkit.
-# 
+#
 #  This file may be used under the terms of the GNU General Public
 #  License version 2.0 as published by the Free Software Foundation
 #  and appearing in the file LICENSE.GPL included in the packaging of
 #  self file.  Please review the following information to ensure GNU
 #  General Public Licensing requirements will be met:
 #  http://www.trolltech.com/products/qt/opensource.html
-# 
+#
 #  If you are unsure which license is appropriate for your use, please
 #  review the following information:
 #  http://www.trolltech.com/products/qt/licensing.html or contact the
 #  sales department at sales@trolltech.com.
-# 
+#
 #  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 #  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-# 
+#
 ############################################################################
 
 import sys
@@ -55,14 +55,6 @@ class RenderThread(QtCore.QThread):
 
         for i in range(RenderThread.ColormapSize):
             self.colormap.append(self.rgbFromWaveLength(380.0 + (i * 400.0 / RenderThread.ColormapSize)))
-
-    def __del__(self):
-        self.mutex.lock()
-        self.abort = True
-        self.condition.wakeOne()
-        self.mutex.unlock()
-
-        self.wait()
 
     def render(self, centerX, centerY, scaleFactor, resultSize):
         locker = QtCore.QMutexLocker(self.mutex)
@@ -200,7 +192,7 @@ class MandelbrotWidget(QtGui.QWidget):
         self.pixmapScale = DefaultScale
         self.curScale = DefaultScale
 
-        self.connect(self.thread, 
+        self.connect(self.thread,
                      QtCore.SIGNAL("renderedImage(const QImage &, double)"),
                      self.updatePixmap)
 
@@ -315,4 +307,7 @@ if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     widget = MandelbrotWidget()
     widget.show()
-    sys.exit(app.exec_())
+    res = app.exec_()
+    widget.thread.abort = True
+    widget.thread.wait()
+    sys.exit(res)
