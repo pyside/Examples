56dfc1ba5f4f50f2f125e1c29f7fe57a64fa73dc
igor_bogomolov <qivb18@gmail.com>
2011-12-29 21:26:40 +0400

    Fix bug in widgets/styles example
    Signed-off-by: Paulo Alcantara <pcacjr@gmail.com>
    Reviewed-by: Paulo Alcantara <pcacjr@gmail.com>

--- examples/widgets/styles.py
+++ examples/widgets/styles.py
@@ -143,7 +143,7 @@ class WidgetGallery(QtGui.QDialog):
         tableWidget = QtGui.QTableWidget(10, 10)
 
         tab1hbox = QtGui.QHBoxLayout()
-        tab1hbox.setMargin(5)
+        tab1hbox.setContentsMargins(5, 5, 5, 5)
         tab1hbox.addWidget(tableWidget)
         tab1.setLayout(tab1hbox)
 
@@ -158,7 +158,7 @@ class WidgetGallery(QtGui.QDialog):
                               "How I wonder what you are!\n")
 
         tab2hbox = QtGui.QHBoxLayout()
-        tab2hbox.setMargin(5)
+        tab2hbox.setContentsMargins(5, 5, 5, 5)
         tab2hbox.addWidget(textEdit)
         tab2.setLayout(tab2hbox)
 
