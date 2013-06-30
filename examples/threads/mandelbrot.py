00537a579f05089f558779e7a35ee446af389810
Jeroen <osscca.git@gmail.com>
2013-06-30 23:55:12 +0200

    Additional corrections.

--- examples/threads/mandelbrot.py
+++ examples/threads/mandelbrot.py
@@ -160,22 +160,22 @@ class RenderThread(QtCore.QThread):
         g = 0.0
         b = 0.0
 
-        if wave >= 380.0 and wave <= 440.0:
+        if 380.0 <= wave <= 440.0:
             r = -1.0 * (wave - 440.0) / (440.0 - 380.0)
             b = 1.0
-        elif wave >= 440.0 and wave <= 490.0:
+        elif 440.0 <= wave <= 490.0:
             g = (wave - 440.0) / (490.0 - 440.0)
             b = 1.0
-        elif wave >= 490.0 and wave <= 510.0:
+        elif 490.0 <= wave <= 510.0:
             g = 1.0
             b = -1.0 * (wave - 510.0) / (510.0 - 490.0)
-        elif wave >= 510.0 and wave <= 580.0:
+        elif 510.0 <= wave <= 580.0:
             r = (wave - 510.0) / (580.0 - 510.0)
             g = 1.0
-        elif wave >= 580.0 and wave <= 645.0:
+        elif 580.0 <= wave <= 645.0:
             r = 1.0
             g = -1.0 * (wave - 645.0) / (645.0 - 580.0)
-        elif wave >= 645.0 and wave <= 780.0:
+        elif 645.0 <= wave <= 780.0:
             r = 1.0
 
         s = 1.0
